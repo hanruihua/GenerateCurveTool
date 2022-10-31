2d32a014bb87a8b2f0124bfd965dfec4ca6b0796
Han <744475118@qq.com>
2022-10-31 21:44:32 +0800

    add dubins path

--- GCT/curve/dubins_path.py
+++ GCT/curve/dubins_path.py
@@ -6,6 +6,8 @@ code author: Han Ruihua
 reference: paper 'Classification of the Dubins set, 2001', github https://github.com/AndrewWalker/Dubins-Curves/blob/master/src/dubins.c
 """
 
+steer_dict = {'L': 1, 'R': -1, 'S': 0 }  # turn left right, straight 
+
 def generate_dubins_path(start=np.zeros((3, 1)), end=np.ones((3, 1)), min_radius=1, step_size=0.1):
 
     """
@@ -38,11 +40,11 @@ def generate_dubins_path(start=np.zeros((3, 1)), end=np.ones((3, 1)), min_radius
 
     return path_point_list
 
-def preprocess(self, start, end, min_radius):
+def preprocess(start, end, min_radius):
         
     assert start.shape == (3, 1) and end.shape == (3, 1)
 
-    dis, radian = self.relative(start[0:2], end[0:2])
+    dis, radian = relative(start, end)
 
     d = dis / min_radius
 
@@ -59,97 +61,147 @@ def path_generate(start, t, p, q, mode, min_radius, step_size):
 
     length = [t, p, q]
 
-    path_point_list = [start]
+    trajectory = [start]
     init_point = start
 
     for i in range(3):
         if length[i] != 0:
-            path_list, end_point = element_sample(length[i], init_point, mode[i], min_radius, step_size)
-            path_point_list = path_point_list + path_list
+            point_list, end_point = element_sample(length[i], init_point, mode[i], min_radius, step_size)
+            trajectory = trajectory + point_list
             init_point = end_point
 
-    return path_point_list
+    return trajectory
 
 def element_sample(length, start_point, steer_mode, min_radius, step_size):
     # generate a series points from the an element
+    
+    # if steer_mode == 'L': steer = 1
+        
+    # elif steer_mode == 'R': steer = -1
+        
+    # if steer_mode == 'S': steer = 0
 
-    cur_x = start_point[0, 0]
-    cur_y = start_point[1, 0]
-    cur_theta = start_point[2, 0]
-
-    path_list = []
-
-    endpoint = np.zeros((3, 1))
-
-    if steer_mode == 'L':
-        steer = 1
-    elif steer_mode == 'R':
-        steer = -1
-    if steer_mode == 'S':
-        steer = 0
-
-    curvature = steer * 1/ min_radius
+    steer = steer_dict[steer_mode]
     real_length = length * min_radius
 
-    rot_theta = real_length * curvature
+    # calculate the end point first
+    # end_pose = trans_pose(start_point, real_length, steer, min_radius)
 
-    # calculate end point
-    if curvature == 0:
-        endpoint = trans_point(start_point, cur_theta, real_length)
+    # generate a series of sample points 
+    cur_length = 0
+    path_list = []
 
-        # end_x = cur_x + cos(cur_theta) * real_length
-        # end_y = cur_y + sin(cur_theta) * real_length
+    while cur_length <= real_length - step_size:
+        cur_length = cur_length + step_size
+        next_pose = trans_pose(start_point, cur_length, steer, min_radius)
+        path_list.append(next_pose)
+
+    end_pose = trans_pose(start_point, real_length, steer, min_radius)
+
+    # add the end pose under current transform
+    if np.linalg.norm(end_pose - path_list[-1]) <= 0.01:
+        path_list[-1] = end_pose
+    else:
+        path_list.append(end_pose)
+
+    return path_list, end_pose
+        
+def trans_pose(start_pose, length, steer, min_radius):
+    # pose: x, y, theta, 3*1
+    # rotate the start pose with a rot_theta by a center point
+
+    cur_theta = start_pose[2, 0]
+    start_position = start_pose[0:2]
+    rot_theta = steer * length / min_radius
+
+    if rot_theta == 0:
+        trans_matrix = length * np.array([ [cos(cur_theta)], [sin(cur_theta)], [0]])
+        end_pose = start_pose + trans_matrix
     else:
         center_theta = cur_theta + steer*pi/2
-        center_x = cur_x + cos(center_theta) * min_radius
-        center_y = cur_y + sin(center_theta) * min_radius
+        center_position = start_position + min_radius * np.array([ [cos(center_theta)], [sin(center_theta)]])
+        rot_matrix = np.array([[cos(rot_theta), -sin(rot_theta)], [sin(rot_theta), cos(rot_theta)]])
+        end_position = center_position + rot_matrix @ (start_position - center_position)
+        end_theta = wraptopi(cur_theta + rot_theta)
+        end_pose = np.vstack((end_position, [end_theta]))
 
-        center_point = np.array( [ [center_x], [center_y], [center_theta] ])
+    return end_pose
 
-        end_cir_theta = cur_theta + rot_theta - steer*pi/2
-        endpoint = trans_point(center_point, end_cir_theta, min_radius)
 
-    cur_length = 0
+
+# def element_sample(length, start_point, steer_mode, min_radius, step_size):
+#     # generate a series points from the an element
+
+#     cur_x = start_point[0, 0]
+#     cur_y = start_point[1, 0]
+#     cur_theta = start_point[2, 0]
+
+#     path_list = []
+
+#     endpoint = np.zeros((3, 1))
+
+#     if steer_mode == 'L':
+#         steer = 1
+#     elif steer_mode == 'R':
+#         steer = -1
+#     if steer_mode == 'S':
+#         steer = 0
+
+#     curvature = steer * 1/ min_radius
+#     real_length = length * min_radius
+
+#     rot_theta = real_length * curvature
+
+#     # calculate end point
+#     if curvature == 0:
+#         endpoint = trans_point(start_point, cur_theta, real_length)
+#     else:
+#         center_theta = cur_theta + steer*pi/2
+#         center_x = cur_x + cos(center_theta) * min_radius
+#         center_y = cur_y + sin(center_theta) * min_radius
+
+#         center_point = np.array( [ [center_x], [center_y], [center_theta] ])
+
+#         end_cir_theta = cur_theta + rot_theta - steer*pi/2
+#         endpoint = trans_point(center_point, end_cir_theta, min_radius)
+
+#     cur_length = 0
     
-    # loop to generate the points
-    while cur_length <= real_length - step_size:
+#     # loop to generate the points
+#     while cur_length <= real_length - step_size:
         
-        next_theta = cur_theta + curvature * step_size
-        cur_length = cur_length + step_size
+#         next_theta = cur_theta + curvature * step_size
+#         cur_length = cur_length + step_size
 
-        if curvature == 0:
-            next_x = cur_x + cos(next_theta) * cur_length
-            next_y = cur_y + sin(next_theta) * cur_length
-        else:
-            temp_theta = next_theta - steer*pi/2
-            next_x = center_x + cos(temp_theta) * min_radius
-            next_y = center_y + sin(temp_theta) * min_radius
+#         if curvature == 0:
+#             next_x = cur_x + cos(next_theta) * cur_length
+#             next_y = cur_y + sin(next_theta) * cur_length
+#         else:
+#             temp_theta = next_theta - steer*pi/2
+#             next_x = center_x + cos(temp_theta) * min_radius
+#             next_y = center_y + sin(temp_theta) * min_radius
 
-        next_theta = wraptopi(next_theta)
+#         next_theta = wraptopi(next_theta)
 
-        next_point = np.array([[next_x], [next_y], [next_theta]])
+#         next_point = np.array([[next_x], [next_y], [next_theta]])
 
-        path_list.append(next_point)
+#         path_list.append(next_point)
 
-        cur_theta = next_theta
+#         cur_theta = next_theta
 
-    path_list.append(endpoint)
+#     path_list.append(endpoint)
 
-    return path_list, endpoint
+#     return path_list, endpoint
 
-def trans_point(start_point, theta, length):
-    trans_matrix = np.array([ [ cos(theta) * length ], [ sin(theta) * length ], [ theta ] ])
-    end_point = start_point + trans_matrix
-    end_point[2, 0] = wraptopi(end_point[2, 0])
+# def trans_point(start_point, theta, length):
 
-    return end_point
+#     trans_matrix = np.array([ [ cos(theta) * length ], [ sin(theta) * length ], [ theta ] ])
+#     end_point = start_point + trans_matrix
+#     end_point[2, 0] = wraptopi(end_point[2, 0])
 
+#     return end_point
 
-
-
-
-
-def dubins_LSL(self, alpha, beta, d):
+def dubins_LSL(alpha, beta, d):
 
     mode = ['L', 'S', 'L']
 
@@ -259,7 +311,7 @@ def dubins_LRL(alpha, beta, d):
 
 def relative(state1, state2):
         
-    diff = state2 - state1
+    diff = state2[0:2] - state1[0:2]
 
     dis = np.linalg.norm(diff)
     radian = atan2(diff[1, 0], diff[0, 0])
--- GCT/curve_generator.py
+++ GCT/curve_generator.py
@@ -1,8 +1,11 @@
+from tkinter import Scale
+import numpy as np
+from GCT.curve.dubins_path import generate_dubins_path
+import matplotlib.pyplot as plt
+from math import cos, sin
 
 class curve_generator:
-    def __init__(self, way_points=[], select_mode='default', curve_style='line', min_radius=0, point_style='pose') -> None:
-        
-        # way_points: The list of way points
+    def __init__(self, select_mode='default', curve_style='line', point_style='pose') -> None:
         
         # select_mode: mode of point selection
             # 'default', use the input point_list
@@ -17,21 +20,82 @@ class curve_generator:
             # position: x,y 2 * 1 matrix
             # pose: x, y, theta, 3*1 matrix, include orientation
 
-        self.way_points = way_points
         self.select_mode = select_mode
         self.curve_style = curve_style
         self.point_style = point_style
-        self.min_radius = min_radius
-    
-    def generate_curve(self, step_size):
         
-        if self.select_mode == 'default':
-            pass
+    def generate_curve(self, way_points =[], step_size = 0.1, min_radius=1, **kwargs):
         
-        elif self.select_mode == 'mouse':
+        # way_points: The list of way points
+        # step_size: distance between the sample points
+
+        # min_radius: minimum turning radius for ackermann robot
+        # curve: a list of points
+
+        # curve_style: 
+        #   - dubins: dubins curve (point_style must be pose)
+
+        
+        self.way_points = way_points
+
+        curve = [ way_points[0] ]
+
+        if self.select_mode == 'mouse':
             pass
         
         if self.curve_style == 'dubins':
-            pass
+            
+            for i in range(len(way_points) - 1):
+                start_point = way_points[i]
+                end_point = way_points[i+1]
+                single_curve = generate_dubins_path(start_point, end_point, min_radius, step_size)
+                curve = curve + single_curve[1:]
 
-    # def 
+        return curve
+
+    def plot_curve(self, curve, style='-g', show_way_points=True, show_direction=False, range_x = [0, 10], range_y = [0, 10], **kwargs):
+        
+        plt.xlim(range_x)
+        plt.ylim(range_y)
+
+        path_x_list = [p[0, 0] for p in curve]
+        path_y_list = [p[1, 0] for p in curve]
+
+        line = plt.plot(path_x_list, path_y_list, style, **kwargs)
+
+        if show_way_points:
+            px_list = [p[0, 0] for p in self.way_points]
+            py_list = [p[1, 0] for p in self.way_points]
+
+            wu_list = [cos(p[2, 0]) for p in self.way_points]
+            wy_list = [sin(p[2, 0]) for p in self.way_points]
+           
+            plt.quiver(px_list, py_list, wu_list, wy_list, color='r', scale=20)
+
+        if show_direction:
+
+            u_list = [cos(p[2, 0]) for p in curve]
+            y_list = [sin(p[2, 0]) for p in curve]
+           
+            plt.quiver(path_x_list, path_y_list, u_list, y_list, color='k', scale=45, scale_units='height')
+    
+
+if __name__ == '__main__':
+
+    point1 = np.array([ [1], [5], [0]])
+    point2 = np.array([ [5], [3], [0]])
+
+    point_list = [point1, point2]
+
+    cg = curve_generator(curve_style='dubins')
+    curve = cg.generate_curve(point_list, 0.1, 2)
+    cg.plot_curve(curve, show_direction=True)
+
+    for i in range(len(curve) - 1):
+
+        distance = round(np.linalg.norm( curve[i+1][0:2] - curve[i][0:2] ), 2)
+
+        print(distance)
+
+    
+    plt.show()
