295f5239dfb01656944d6c7dece1d3ec97a0b0fe
Herry_robot <744475118@qq.com>
2023-01-29 23:37:23 +0800

    example

---
+++ example/dubins_curve.py
@@ -0,0 +1,22 @@
+from GCT.curve_generator import curve_generator
+import matplotlib.pyplot as plt
+import numpy as np
+
+if __name__ == '__main__':
+
+    point1 = np.array([ [1], [5], [0]])
+    point2 = np.array([ [5], [3], [0]])
+    point3 = np.array([ [6], [5], [3]])
+    point4 = np.array([ [2], [2], [2]])
+
+    point_list = [point1, point2, point3, point4]
+
+    cg = curve_generator()
+
+    curve = cg.generate_curve('dubins', point_list, 0.1, 2)
+
+    print(curve)
+
+    if curve is not None: cg.plot_curve(curve, show_direction=False)
+
+    plt.show()
---
+++ example/mouse_curve.py
@@ -0,0 +1,15 @@
+from GCT.curve_generator import curve_generator
+import matplotlib.pyplot as plt
+import numpy as np
+
+if __name__ == '__main__':
+
+    point_list = []
+
+    cg = curve_generator(select_mode='mouse')
+
+    curve = cg.generate_curve('dubins', point_list, 0.1, 2)
+
+    if curve is not None: cg.plot_curve(curve, show_direction=False)
+
+    plt.show()
